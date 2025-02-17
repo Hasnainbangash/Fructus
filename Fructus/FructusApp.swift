cc4f2d1ca4b7352199b291c3cd9714304cd5a8a4
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-17 14:37:54 +0500

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Elexoft on 17/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
