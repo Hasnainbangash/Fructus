430f40bae3ed109803b24601c3726cd92c828099
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-17 15:16:23 +0500

    Fruit title text properties are added

--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -23,6 +23,10 @@ struct FruitCardView: View {
                 
                 // FRUIT: TITLE
                 Text("Blueberry")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0,green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
                 // FRUIT: HEADLINE
                 // BUTTON: START
             } //: VSTACK
