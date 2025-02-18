892e66eadc4889c309132c2f2e9f429ee3db7043
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-18 10:26:01 +0500

    Fruit nutrints view work done

--- Fructus/View/FruitNutrientsView.swift
+++ Fructus/View/FruitNutrientsView.swift
@@ -19,10 +19,19 @@ struct FruitNutrientsView: View {
         GroupBox {
             DisclosureGroup("Nutritional value per 100g") {
                 ForEach(0..<nutrients.count, id: \.self) { item in
+                    Divider().padding(.vertical, 2)
                     HStack {
-                        Text(nutrients[item])
-                        Spacer()
+                        Group {
+                            Image(systemName: "info.circle")
+                            Text(nutrients[item])
+                        }
+                        .foregroundColor(fruit.gradientColors[1])
+                        .font(Font.system(.body).bold())
+                        
+                        Spacer(minLength: 25)
+                        
                         Text(fruit.nutrition[item])
+                            .multilineTextAlignment(.trailing)
                     }
                 }
             }
