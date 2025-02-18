1ecdb144c21c796499d0f992d20190f3bb032798
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-18 10:35:51 +0500

    Settings view screen is added to the ptroject

---
+++ Fructus/App/SettingsView.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsView.swift
+//  Fructus
+//
+//  Created by Elexoft on 18/02/2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    // MARK: - PROPERTIES
+    
+    
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+#Preview {
+    SettingsView()
+}
