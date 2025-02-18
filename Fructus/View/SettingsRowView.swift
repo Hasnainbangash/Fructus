e1be6e1b1604710156a434f530c8e8ab2150c7bb
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-18 11:52:00 +0500

    Settings row view work done along with the simple comtent and with the link content

--- Fructus/View/SettingsRowView.swift
+++ Fructus/View/SettingsRowView.swift
@@ -11,7 +11,9 @@ struct SettingsRowView: View {
     // MARK: - PROPERTIES
     
     var name: String
-    var content: String
+    var content: String? = nil
+    var linkLabel: String? = nil
+    var linkDestination: String? = nil
     
     // MARK: - BODY
     
@@ -19,14 +21,27 @@ struct SettingsRowView: View {
         HStack {
             Text(name).foregroundColor(Color.gray)
             Spacer()
-            Text(content)
+            if (content != nil) {
+                Text(content!)
+            } else if (linkLabel != nil && linkDestination != nil) {
+                Link(linkLabel!, destination: URL(string: "https://\(linkDestination!)")!)
+                Image(systemName: "arrow.up.right.square").foregroundColor(.pink)
+            } else {
+                EmptyView()
+            }
         }
     }
 }
 
 // MARK: - PREVIEW
-#Preview {
+#Preview("Simple Preview") {
     SettingsRowView(name: "Developer", content: "John / Jane")
+        .previewLayout(.fixed(width: 375, height: 60))
+        .padding()
+}
+
+#Preview("Link Preview") {
+    SettingsRowView(name: "Developer", linkLabel: "SwiftUI Masterclass", linkDestination: "swiftuimasterclass.com")
         .previewLayout(.fixed(width: 375, height: 60))
         .padding()
 }
